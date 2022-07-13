d05a8100107fb423be358d283216837c9bd4c6e3
LukeXu3543 <106456758+LukeXu3543@users.noreply.github.com>
2022-07-12 21:18:11 -0400

    Delete marital-status.sql

--- marital-status.sql
+++
@@ -1,7 +0,0 @@
-/* The result shows that ALL customers who have a high spending score
-are married, indicating a very strong relationship between customers' interests in
-automobile and their marital status*/
-SELECT ever_married, COUNT(spending_score) AS marriage_spending FROM customers
-WHERE spending_score = 'High' AND ever_married IS NOT NULL
-GROUP BY ever_married 
-ORDER BY marriage_spending DESC
